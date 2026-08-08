e8d4e243a32ddddb62cda244cff3e9082cb989c8
Caio Marques <cmls.marques@gmail.com>
2026-08-08 14:20:42 -0300

    adicionando a suite de testes de delete e o teste de health da api

---
+++ test/health.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const { expect } = require('chai');
+require('dotenv').config();
+
+describe('Health Check', () => {
+    describe('GET /health', () => {
+        it('Deve retornar 200 e informar que a API está disponível', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/health');
+
+            expect(resposta.status).to.equal(200);
+            expect(resposta.headers['content-type']).to.include('application/json');
+            expect(resposta.body).to.deep.equal({ status: 'ok' });
+        });
+    });
+});
